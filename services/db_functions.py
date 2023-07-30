d63632d09162f6a314a1e1c6413427c550b9a10c
xeond8 <levpyatko1111@gmail.com>
2023-07-30 06:09:11 +0300

    Update db_functions.py

--- services/db_functions.py
+++ services/db_functions.py
@@ -11,21 +11,21 @@ def update_text(text: str):
 
 def geocoder(name: str, city: str, address: str) -> tuple:
     req_url: str = 'https://nominatim.openstreetmap.org/search'
-    params1: dict[str, Any] = {'city': city, 'street': address, 'format': 'json', 'addressdetails': 1}
-    params2: dict[str, Any] = {'city': city, 'street': name, 'format': 'json', 'addressdetails': 1}
+    params1: dict = {'city': city, 'street': address, 'format': 'json', 'addressdetails': 1}
+    params2: dict = {'city': city, 'street': name, 'format': 'json', 'addressdetails': 1}
     if requests.get(req_url, params=params1).text != '[]':
-        shop_info: dict[str, Any] = json.loads(requests.get(req_url, params=params1).text)[0]
+        shop_info: dict = json.loads(requests.get(req_url, params=params1).text)[0]
         return float(shop_info['lat']), float(shop_info['lon'])
     elif requests.get(req_url, params=params2).text != '[]':
-        shop_info: dict[str, Any] = json.loads(requests.get(req_url, params=params1).text)[0]
+        shop_info: dict = json.loads(requests.get(req_url, params=params1).text)[0]
         return float(shop_info['lat']), float(shop_info['lon'])
     else:
         return 0, 0
 
 
-async def save_to_database(data: dict[str, Any], user_id: str) -> None:
+async def save_to_database(data: dict, user_id: str) -> None:
     with open("services/coffeeshops.json", "r") as file:
-        database: dict[str, dict] = json.loads(file.read())
+        database: dict = json.loads(file.read())
     cafe_name: str = data.pop('name')
     lat, lon = geocoder(cafe_name, data['city'], data['address'])
     data['lat'] = lat
@@ -37,11 +37,11 @@ async def save_to_database(data: dict[str, Any], user_id: str) -> None:
         json.dump(database, file)
 
 
-def print_entry(entry: tuple[str, float], lg_code: str, user_id: str) -> tuple[str, str]:
+def print_entry(entry: tuple, lg_code: str, user_id: str) -> tuple:
     name, dist = entry[0], str(round(entry[1], 1)).replace('.', '\.')
     with open("services/coffeeshops.json", "r") as file:
-        database: dict[str, dict] = json.loads(file.read())
-    cshop: dict[str, Any] = database[user_id][name]
+        database: dict = json.loads(file.read())
+    cshop: dict = database[user_id][name]
 
     if lg_code == 'ru':
         entry_string: str = f'{name}\n📍{cshop["address"] + ", " + cshop["city"]}\n{dist} км от вас\n\n{cshop["description"]}\n\nРейтинг: {cshop["rating"]}⭐'
@@ -61,11 +61,11 @@ def count_distance(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
 
 async def find_three_nearest(lat1: float, lon1: float, user_id: str):
     with open("services/coffeeshops.json", "r") as file:
-        database: dict[str, dict] = json.loads(file.read())
+        database: dict = json.loads(file.read())
     dist_ratio = []
     user_database = database[user_id]
     for name, cshop in user_database.items():
         lat2, lon2 = cshop['lat'], cshop['lon']
         dist = count_distance(lat1, lon1, lat2, lon2)
-        dist_ratio.append(tuple([name, dist]))
+        dist_ratio.append(tuple)
     return sorted(dist_ratio, key=lambda x: x[1])[:3]
